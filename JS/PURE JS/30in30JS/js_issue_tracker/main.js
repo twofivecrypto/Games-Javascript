cd3c5e08542fcf3ea395a83ec617a1754106b79b
Mansa <twofivecrypto@gmail.com>
2023-06-04 04:13:54 -0400

    todo: input delete buttons

---
+++ JS/PURE JS/30in30JS/js_issue_tracker/main.js
@@ -0,0 +1,50 @@
+
+
+function fetchIssues () {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById('issuesList');
+
+    issuesList.innerHTML +=   '<div class="well">'+
+    '<h6>Issue ID: ' + id + '</h6>'+
+    '<p><span class="label label-info">' + status + '</span></p>'+
+    '<h3>' + desc + '</h3>'+
+    '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' '+
+    '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+    '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> '+
+    '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'+
+    '</div>';
+}
+
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+
+function saveIssue(e) {
+    var issueID = chance.guid();
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueStatus = 'Open';
+
+    var issue = {
+        id: issueID,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    if (localStorage.getItem('issues') === null) {
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issues);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+
+    document.getElementById('issueInputForm').reset();
+
+    fetchIssues();
+    e.preventDefault();
+}
